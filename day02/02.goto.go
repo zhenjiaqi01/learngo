694f1af36bfea9d5f658bc22eb4973ca94ac5520
xmd <243464201@qq.com>
2021-12-12 11:26:34 +0800

    第一次提交信息（工作中我们一般写我们修改了哪些代码）

--- day02/02.goto.go
+++ day02/02.goto.go
@@ -1,8 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 	//标签label1
 	//goto label1
 	//break label1
 	//continue label1
+LABEL:
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if j == 3 {
+				//goto LABEL
+				//continue LABEL
+				break LABEL
+			}
+			fmt.Println("i: ", i, "j: ", j)
+		}
+	}
 }
